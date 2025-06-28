ed0129921d502c023c4e7b749660ba6998d5e3e4
Philip Stray <philip.stray@gmail.com>
2025-06-28 12:26:25 +0200

    test commit

--- src/react-app/App.tsx
+++ src/react-app/App.tsx
@@ -1,10 +1,6 @@
 // src/App.tsx
 
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import cloudflareLogo from "./assets/Cloudflare_Logo.svg";
-import honoLogo from "./assets/hono.svg";
 import "./App.css";
 
 function App() {
@@ -13,35 +9,19 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-        <a href="https://hono.dev/" target="_blank">
-          <img src={honoLogo} className="logo cloudflare" alt="Hono logo" />
-        </a>
-        <a href="https://workers.cloudflare.com/" target="_blank">
-          <img
-            src={cloudflareLogo}
-            className="logo cloudflare"
-            alt="Cloudflare logo"
-          />
-        </a>
-      </div>
-      <h1>Vite + React + Hono + Cloudflare</h1>
-      <div className="card">
+     <div>
         <button
           onClick={() => setCount((count) => count + 1)}
           aria-label="increment"
         >
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <button
+          onClick={() => setCount(() => 0)}
+          aria-label="reset"
+        >
+          reset count
+        </button>
       </div>
       <div className="card">
         <button
@@ -54,11 +34,7 @@ function App() {
         >
           Name from API is: {name}
         </button>
-        <p>
-          Edit <code>worker/index.ts</code> to change the name
-        </p>
       </div>
-      <p className="read-the-docs">Click on the logos to learn more</p>
     </>
   );
 }
